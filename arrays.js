bf3284d5117b9fe3a92b5f1fbe235c41c64597b0
Chris Albanese <chris@reaorlando.com>
2017-11-30 23:28:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
-  var chocolateBars = ["snickers", "hundred grand", "kitkat", and "skittles"];
+var chocolateBars = ["snickers", "hundred grand", "kitkat", and "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   array = [0,1,2,3];
   element = "beginning"
   var newArray = [element...array];
 
-} 
+}
