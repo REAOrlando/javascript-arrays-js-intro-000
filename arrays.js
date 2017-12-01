25a892e20409ab95c3f6f0c269c189c2d0705f22
Chris Albanese <chris@reaorlando.com>
2017-12-01 03:03:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
   function destructivelyAddElementToBeginningOfArray(array, element) {
     // array = [1];
     // element = 'foo';
-    array = [element, ... array];
+  array.unShift(element);
   return array;
   }
 
